ffe92ef3aab58b24eab6302303aadb2dc8cf8989
simchodi <simchodi@gmail.com>
2022-06-09 22:07:59 +0900

    commit Test

--- hello-spring/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -9,6 +9,10 @@ public class HelloSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 		System.out.println("test");
+		System.out.println("test2");
+		System.out.println("reset Test");
+		System.out.println("Stage Test");
 	}
 
 }
+
